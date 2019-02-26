2e5bee921c71c09c00f1af6d7e604197ca29688d
ProgammerMasya <vlad3032001@mail.ru>
2019-02-26 23:08:46 +0300

    first commit

---
+++ ecomapp/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from ecomapp.models import Category, Product, Brand, CartItem, Cart, Order
+
+
+def make_payed(modeladmin, request, queryset):
+    queryset.update(status='Оплачен')
+
+
+make_payed.short_description = "Пометить как оплаченные"
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_filter = ['status']
+    actions = [make_payed]
+
+
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Product)
+admin.site.register(CartItem)
+admin.site.register(Cart)
+admin.site.register(Order, OrderAdmin)
+
---
+++ ecomapp/urls.py
@@ -0,0 +1,41 @@
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LogoutView
+from ecomapp.views import (
+    base_view,
+    category_view,
+    product_view,
+    cart_view,
+    add_to_cart_view,
+    remove_from_cart_view,
+    change_item_qty,
+    checkout_view,
+    order_create_views,
+    make_order_view,
+    account_view,
+    registration_view,
+    login_view
+)
+
+urlpatterns = [
+    re_path(r'^category/(?P<category_slug>[-\w]+)/$', category_view,
+            name='category_detail'),
+    re_path(r'^product/(?P<product_slug>[-\w]+)/$', product_view,
+            name='product_detail'),
+    re_path(r'^add_to_cart/$', add_to_cart_view,
+            name='add_to_cart'),
+    re_path(r'^remove_from_cart/$',
+            remove_from_cart_view, name='remove_from_cart'),
+    path('change_item_qty/', change_item_qty, name='change_item_qty'),
+    path('cart/', cart_view, name='cart'),
+    path('checkout/', checkout_view, name='checkout'),
+    path('order/', order_create_views, name='create_order'),
+    path('make_order/', make_order_view, name='make_order'),
+    path('thank_you/', TemplateView.as_view(template_name="thank_you.html"),
+         name='thank_you'),
+    path('account/', account_view, name='account'),
+    path('registration/', registration_view, name='registration'),
+    path('login/', login_view, name='login'),
+    path('logout/', LogoutView.as_view(next_page='base'), name='logout'),
+    path('', base_view, name='base'),
+]
